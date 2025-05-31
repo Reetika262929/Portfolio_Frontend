f4b4176389965446adfed383d6a9c2192d49ca3e
Reetika1206 <pa.reetika2001@gmail.com>
2025-06-01 00:20:22 +0530

    creater: the skills div with the footer and they are in progress

---
+++ app/Components/ToolingBox/toolBox.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const ToolBox = () => {
+  return (
+    <div style={{ padding: "80px 50px",backgroundColor:'#050810'}}>
+<div className='tooling-box'>
+TOOLING
+</div>
+
+<div className='tooling-box1'>
+    <div className='box-content'>
+        React
+    </div>
+
+     <div className='box-content'>
+        Angular
+    </div>
+     <div className='box-content'>
+        HTML
+    </div>
+     <div className='box-content'>
+        CSS
+    </div>
+     <div className='box-content'>
+        JavaScript
+    </div>
+    <br />
+    <div className='box-content'>
+        React
+    </div>
+
+     <div className='box-content'>
+        Angular
+    </div>
+     <div className='box-content'>
+        HTML
+    </div>
+     <div className='box-content'>
+        CSS
+    </div>
+     <div className='box-content'>
+        JavaScript
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default ToolBox
--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import "./globals.css"
 import TopBar from "./Components/Topbar/topbar";
 import MainSection from "./Components/mainSection/mainSection";
+import ToolBox from "./Components/ToolingBox/toolBox";
+import Footer from "./Components/Footer/footer";
 // import styles from "./page.module.css";
 
 export default function Home() {
@@ -9,6 +11,10 @@ export default function Home() {
   <>
   {/* <TopBar /> */}
   <MainSection />
+  <ToolBox />
+
+
+  <Footer />
   </>
   );
 }
